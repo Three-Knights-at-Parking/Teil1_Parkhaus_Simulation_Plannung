7bccd42809ec33a539a62aa846c41dcf0129a5f2
maupher <ibach.simon-it25@dhbw-ravensburg.de>
2026-02-26 17:48:23 +0100

    test building code for an overview. Adding ideas and recommendations to types.h

--- include/types.h
+++ include/types.h
@@ -13,56 +13,68 @@ typedef void (*SimulationTickFunction)(SimulationObject *p_self, uint32_t curren
 /**
  * Base Simulation Object for polymorphism.
  */
-// Eine simulation simuliert immer das selbe Szenario / Zeitfenster, lediglich mit unterschiedlichen Parkhaus formaten
 struct SimulationObject {
     int id;
-    tick_t current_time; // Current tick time.
-    tick_t end_time;
     SimulationTickFunction tick;
 };
-//---------------------------------------
-// alles auf Ticks anpassen?????
-//--> Einfachere kodierung und nur speichern des Ticks -> Kein umrechnen while runtime nötig von Ticks in Uhrzeit
-
-//generalisierte definition for easy adjustment
-typedef uint32_t tick_t;
-typedef uint16_t ParkingSpaces;
-//---------------------------------------
-
+//typedef tick_t unit32_t;
+//typedef places_p unit16_t
 
 typedef struct Parkhaus {
     SimulationObject base; // base object
     char name[20]; // FIXME size is currently arbitrary, we should probably move this to a defined constant in the future
-     uint8_t floors; // Number of floors. This is currently miscellaneous
-
-    ParkingSpaces size; // Number of total parking spaces
-    ParkingSpaces fill_size; // Number of slots filled
-    ParkingSpaces queue_size; // Number of cars in queue
-
-//########## WRONG HERE ###########################
-    ParkingSpaces current_slot; // *
-
+    uint32_t current_time; // Current tick time.
+    uint16_t size; // Number of total parking spaces
+    uint8_t floors; // Number of floors. This is currently miscellaneous
+    uint16_t fill_size; // Number of slots filled
+    uint16_t queue_size; // Number of cars in queue
 }Parkhaus;
 
 typedef struct Car {
     SimulationObject base; // base object
-    tick_t created_at; // Time created
-    tick_t queue_time; // Time wasted in queue
-    tick_t parking_time; // Current total parking time
-    ParkingSpaces current_slot; // Currently occupied parking spot, 0 if none.
+    uint32_t created_at; // Time created
+    uint32_t queue_time; // Time wasted in queue
+    uint32_t parking_time; // Current total parking time
+    uint16_t current_slot; // Currently occupied parking spot, 0 if none.
     //uint16_t current_floor; // Currently occupied floor, 0 if none or don't care
 }Car;
 
 typedef struct Settings {
     char* src_path; // Relative path to settings file, if any. Settings takes ownership of the string.
-    ParkingSpaces size; // Total parking spots
+    uint16_t size; // Total parking spots
     uint8_t floors; // Number of floors. This is currently miscellaneous
     uint16_t real_equivalent; // Tick equivalent in real time (seconds)
     uint8_t output_mode; // 0 normal, 1 verbose, 2 debug FIXME Needs specific definition @Dani
-    tick_t max_ticks; //--> move to SimObject NOT general Settings!!! Kills the funktion of multiple simulations
-    // Max amount of ticks before the simulation stops. -1 for day equivalent.
+    uint32_t max_ticks; // Max amount of ticks before the simulation stops. -1 for day equivalent.
     int32_t rand_seed; // Specified random seed, -1 if current time should be used.
 }Settings;
 
 
+/* ******************************************************
+ 						ADD
+
+ CAR:
+		- leavingIn_ticks -> Cars
+      	- Benefit : kein hochzählen in car nötig
+      	- further funcionality Auto kann mehr als ein Parkplatz benötigen -> neededSpaces + simulationsvariable in Settings
+ 		- Queue_Entry / Exit
+
+PARKHAUS:
+		- add total_left
+		- add total_entry
+		- add anz. Schranken --> important for Simulation
+
+ SETTINGS:
+	 	- maxParking_Ticks
+			setting files benötig eine einstellung für den ParkingTime generator was die max Parkzeit (in Ticks) beträgt
+ 		- parking_time unnötig --> can be calculated durch created_at--> safe memory
+			parking_time müsste auch hochgezählt werden
+		- BadParking -> warscheinlichkeit
+		- CarEntry-perSec -> Warscheinlichkeit
+			Berrechnung ob car entry für jede Sekunde eines Ticks? Abzüglich einfahrtzeit?
+			Max mögliche einfahrten 1Car pro 4 Sekunden -> Pro schranke
+
+
+ Allgemein bitte alle zeiten in Ticks bennenen -> verhindert verwirrung
+*/
 #endif //TEIL1_PARKHAUS_SIMULATION_PLANNUNG_TYPES_H
--- src/simulation.c
+++ src/simulation.c
@@ -3,4 +3,57 @@
 //
 
 #include "simulation.h"
+#include <stdio.h>
+#include "types.h"
+
+
+ Pseudocode;
+int sim_tick()
+{
+    short status;
+
+    if (get.currentTick + 1 == get_maxTick)
+    {
+        currentTick++;
+        status = endSimulation(currentTick);
+    }
+    else if (get.currentTick < get_maxTick)
+    {
+        currentTick++;
+        status = Parkhaus_Tick(currentTick);
+    }
+    else if (get.currentTick >= get_maxTick)
+    {
+        endSimulation();
+        status = 0;   //error
+    }
+    return status;
+}
+
+int Parkhaus_Tick(uint32_t currentTick)
+{
+    Node* currentNode = head_CarList;
+
+    while (current != NULL) {
+        if ( ((current->car.created_at - currentTick)) < current->car.leavingIn_Ticks) {
+            current = current->next;
+        }
+        else if ( ((current->car.created_at - currentTick)) >= current->car.leavingIn_Ticks)
+        {
+            Car_leaving();
+            current = current->next;
+        }// nächstes Element
+    }
+}
+
+
+int Car_leaving(Parkhaus *p, car *c)
+{
+    if (p == NULL) return 0;
+    if (c == NULL) return 10;
+
+    p->fill_size--;
+    p->total_left++;
+
+}
 
