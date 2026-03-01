d010612a9a02d929771f5f39dfd5c24398972a92
maupher <ibach.simon-it25@dhbw-ravensburg.de>
2026-03-01 03:12:29 +0100

    - Auslagerung - Demandberrechnung pro Tick

---
+++ include/add-headers/demand.h
@@ -0,0 +1,15 @@
+//
+// Created by ibach on 01.03.2026.
+//
+
+#ifndef TEIL1_PARKHAUS_SIMULATION_PLANNUNG_DEMAND_H
+#define TEIL1_PARKHAUS_SIMULATION_PLANNUNG_DEMAND_H
+
+/*
+ * Demand-Modul
+ * - simuliert den gesamten Demand pro Tick (global)
+ */
+
+FUNCTION Demand_GenerateTotalPerTick(settings, current_tick, rng)
+
+#endif //TEIL1_PARKHAUS_SIMULATION_PLANNUNG_DEMAND_H
